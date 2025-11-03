2f7bf01dc15b4e78abd26ebaa6bcf722c1b7801d
GUBBIB <moon.dragon250@gmail.com>
2025-11-03 11:44:56 +0900

    vite 형식 리액트 초기 파일 업로드

---
+++ web/src/componenet/Map.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface MapProps {
+    // Add props here if needed
+}
+
+const Map = () => {
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default Map;
